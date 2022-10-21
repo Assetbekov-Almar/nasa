cc4f1bc23ebff5df508e8ad1007cc50eaa29a811
Assetbekov-Almar <almar.asetbekov@gmail.com>
2022-10-21 15:59:44 +0600

    Feature: Add planets csv data and model

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,26 @@
+const parse = require('csv-parse')
+const fs = require('fs')
+
 const planets = []
 
-module.exports = planets
+function isHabitablePlanet(planet) {
+	return planet['koi_disposition'] === 'CONFIRMED'
+		&& planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+		&& planet['koi_prad'] < 1.6
+}
+
+fs.createReadStream('data/kepler_data.csv')
+	.pipe(parse({
+		comment: '#',
+		columns: true,
+	}))
+	.on('data', (data) => {
+		if (isHabitablePlanet(data)) {
+			planets.push(data)
+		}
+	})
+	.on('error', (err) => {
+		console.log(err)
+	})
+
+module.exports = { planets }
