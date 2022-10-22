8ec4fcfe6265eb4ca8db284000e005db6faebdbf
Assetbekov-Almar <almar.asetbekov@gmail.com>
2022-10-22 12:46:12 +0600

    Feature: Add post route for adding a launch

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,5 +1,7 @@
 const launches = new Map()
 
+let latestFlightNumber = 100
+
 const launch = {
 	flightNumber: 100,
 	mission: 'Kepler Exploration X',
@@ -17,4 +19,16 @@ function getAllLaunches() {
 	return [...launches.values()]
 }
 
-module.exports = { getAllLaunches }
+function addNewLaunch(launch) {
+	latestFlightNumber++
+	launches.set(launch.flightNumber,
+		Object.assign(launch, {
+			success: true,
+			upcoming: true,
+			customer:['Zero to Mastery', 'NASA'],
+			flightNumber: latestFlightNumber}
+		)
+	)
+}
+
+module.exports = { getAllLaunches, addNewLaunch }
